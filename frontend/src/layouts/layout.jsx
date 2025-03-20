ceefbd558b5925cc5e10bc304df3f9a4fb0be28c
leontoys <svliyon@gmail.com>
2025-03-20 13:44:31 +1100

    hero image

--- frontend/src/layouts/layout.jsx
+++ frontend/src/layouts/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Layout = ({children})=>{
     console.log(children)
@@ -6,6 +7,7 @@ const Layout = ({children})=>{
     return(
         <div className="flex flex-col min-h-screen">            
             <Header></Header>
+            <Hero></Hero>
             <div className="container mx-auto flex-1 py-10">
                 {children}
             </div>
