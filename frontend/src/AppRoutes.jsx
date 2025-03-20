1d26d01610f70aa7ac3ac1ce762ef21f3135b69e
leontoys <svliyon@gmail.com>
2025-03-20 12:44:31 +1100

    added layout and header

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./layouts/layout";
 
 const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/" element={<span>Home Page</span>}></Route>
+            <Route path="/" element={<Layout>HomePage</Layout>}></Route>
             <Route path="/user-profile" element={<span>User Profile</span>}></Route>
             <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
         </Routes>
