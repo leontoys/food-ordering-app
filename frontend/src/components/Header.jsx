f0e709f7a8ee842b4e87129edbddb9a7cb5ac4e3
leontoys <svliyon@gmail.com>
2025-03-20 13:30:31 +1100

    added desktop main nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link  } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav  from "./MainNav"
 
 const Header = ()=>{
     return(
@@ -12,6 +13,9 @@ const Header = ()=>{
             <div className="md:hidden">
                 <MobileNav />
             </div> 
+            <div className="hidden md:block">
+                <MainNav></MainNav>
+            </div>
         </div>
     </div>
     )
---
+++ frontend/src/components/MainNav.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+
+const MainNav = ()=>{
+    return(
+    <span className="flex space-x-2 items-center">
+        <Button variant="ghost">
+          Log In
+        </Button>        
+    </span>
+    )
+}
+
+export default MainNav
