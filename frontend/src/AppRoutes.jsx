11cf2baf33e3f3fbac27eeec85c5944325ce5042
leontoys <svliyon@gmail.com>
2025-03-21 09:39:08 +1100

    added-home-page

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/" element={<Layout>HomePage</Layout>}></Route>
+            <Route path="/" element={<Layout><HomePage/></Layout>}></Route>
             <Route path="/user-profile" element={<span>User Profile</span>}></Route>
             <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
         </Routes>
